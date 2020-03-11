ab043fc3f9a9ac8e894eac3b8acf56d3fa9020a5
shenzhijia <shenzhijia@ainirobot.com>
2020-03-11 10:50:05 +0800

    11

--- arithmetic/src/main/java/yingjia67/arithmetic/Test.java
+++ arithmetic/src/main/java/yingjia67/arithmetic/Test.java
@@ -2,25 +2,30 @@ package yingjia67.arithmetic;
 
 public class Test {
 
-    static class Ob{
+   public int maxvalue(int n){
+       if(n == 0 || n ==1)
+           return n;
+       return maxvalue(n-1)+maxvalue(n-2);
+   }
 
-    }
+   public int maxvalue2(int n){
+
+       if(n == 0 || n ==1)
+           return n;
+       int st1,st2;
+       st1 = 0;
+       st2 =1;
+       for(int i = 0;i<n-1;i++){
+            int tmp = st2;
+            st2 = st1+st2;
+            st1 = tmp;
+       }
+       return st2;
+   }
 
     public static void main(String[] args) {
         Test test = new Test();
-        Integer a = 5;
-        test.changeValue(a);
-        System.out.println(a);
-//        Ob ob = null;
-//        test.changeValue(ob);
-//        System.out.println(ob);
-
-
-    }
-    public void changeValue(Integer a){
-        a = 4;
-    }
-    public void changeValue(Ob a){
-        a = new Ob();
+        System.out.println(test.maxvalue(18));
+        System.out.println(test.maxvalue2(18));
     }
 }
---
+++ arithmetic/src/main/java/yingjia67/arithmetic/dynamicProgramming/LongestSubstring.java
@@ -0,0 +1,12 @@
+package yingjia67.arithmetic.dynamicProgramming;
+
+public class LongestSubstring {
+    static class Solution {
+        public int longestSubstring(String s, int k) {
+
+
+
+            return 0;
+        }
+    }
+}
